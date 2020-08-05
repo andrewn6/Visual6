db8141b1b1be1ffdb6ab92ef4af04785f10ffeb0
FFlop <andrewnijmeh1@gmail.com>
2020-08-04 20:41:07 -0400

    Created __init__ to treat the directories as packages

---
+++ cogs/utils/__init__.py
@@ -0,0 +1,6 @@
+from .commands import Commands
+from .hypixelstats import Hypixelstats
+from .modcommands import Mod
+from .music import Music
+from .errorHandler import Errors
+from .checks import * 
